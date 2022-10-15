854aad21e568eb03a7db176fcb240aca9d06efcc
Luiz Fernando - Casa <36427462+LuizFernandoSousa@users.noreply.github.com>
2022-10-15 00:27:16 -0300

    Aula 14/10/2022

--- FilmesAPI/FilmesAPI/Controllers/FilmeController.cs
+++ FilmesAPI/FilmesAPI/Controllers/FilmeController.cs
@@ -1,4 +1,7 @@
-﻿using FilmesAPI.Models;
+﻿using AutoMapper;
+using FilmesAPI.Data;
+using FilmesAPI.Data.Dtos;
+using FilmesAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -13,37 +16,77 @@ namespace FilmesAPI.Controllers
     [Route("[controller]")]
     public class FilmeController : ControllerBase
     {
-        private static List<Filme> filmes = new List<Filme>();
-        private static int Id = 1;
+        private FilmeContext _context;
+        private IMapper _mapper;
+        public FilmeController(FilmeContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
 
         [HttpPost]
-        public IActionResult AdicionaFilme([FromBody]Filme filme)
+
+        public IActionResult AdicionaFilme([FromBody] CreateFilmeDto filmedto)
         {
-            filme.Id = Id++;
-            filmes.Add(filme);
-            return CreatedAtAction(nameof(RecuperaFilmesPorID),new {id = filme.Id},filme);
+            Filme filme = _mapper.Map<Filme>(filmedto);
+
+
+            _context.Filmes.Add(filme);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(RecuperaFilmesPorID), new { id = filme.Id }, filme);
         }
 
         [HttpGet]
         public IActionResult RecuperaFilmes()
         {
-            return Ok(filmes);
+            return Ok(_context.Filmes);
         }
 
         [HttpGet("{id}")]
         public IActionResult RecuperaFilmesPorID(int id)
         {
 
-            Filme filme = filmes.FirstOrDefault(filmes => filmes.Id == id);
+            Filme filme = _context.Filmes.FirstOrDefault(filmes => filmes.Id == id);
 
             if (filme != null)
             {
-                return Ok(filme); 
+                ReadFilmeDto filmeDto = _mapper.Map<ReadFilmeDto>(filme);
+
+                return Ok(filme);
             }
 
             return NotFound();
+        }
 
-            
+
+        [HttpPut("{id}")]
+        public IActionResult AtualizarFilme(int id, [FromBody] UpdateFilmeDto filmeDto)
+        {
+            Filme filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+            if (filme==null)
+            {
+                return NotFound();
+            }
+
+            _mapper.Map(filmeDto,filme);
+            _context.SaveChanges();
+            return NoContent(); 
+
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeletaFilme(int id)
+        {
+            Filme filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+            if (filme == null)
+            {
+                return NotFound();
+            }
+            _context.Remove(filme);
+            _context.SaveChanges();
+            return NoContent();
+
+
         }
 
 
---
+++ FilmesAPI/FilmesAPI/Data/Dtos/ReadFilmeDto.cs
@@ -0,0 +1,30 @@
+﻿using FilmesAPI.Data;
+using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace FilmesAPI.Data.Dtos
+{
+    public class ReadFilmeDto
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "O campo titulo deve ser obrigatorio")]
+        public string Titulo { get; set; }
+        [Required(ErrorMessage = "O campo diretor deve ser obrigatorio")]
+        public string Diretor { get; set; }
+        [StringLength(30, ErrorMessage = "O genêro deve conter no maximo 30 caracteres")]
+        public string Genero { get; set; }
+        [Range(1, 600, ErrorMessage = " A duração deve conter no minimo 1 e no maximo 600 minutos")]
+        public int Duracao { get; set; }
+
+        public DateTime HoraDaConsulta { get; set; }
+
+    }
+}
