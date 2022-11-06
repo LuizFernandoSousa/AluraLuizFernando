7f220171dc813b7f19898b7bce35606a54931089
Luiz Fernando - Casa <36427462+LuizFernandoSousa@users.noreply.github.com>
2022-11-06 16:41:21 -0300

    06/11

---
+++ FilmesAPI/UsuariosAPI/Controllers/CadastroController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MySqlX.XDevAPI.Common;
+using UsuariosAPI.Data.Dtos;
+using UsuariosAPI.Services;
+
+namespace UsuariosAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CadastroController : ControllerBase
+    {
+        private CadastroService _cadastroService;
+
+        public CadastroController(CadastroService cadastroService)
+        {
+            _cadastroService = cadastroService;
+        }
+
+        [HttpPost]
+        public IActionResult CadastraUsuario(CreateUsuarioDto createDto)
+        {
+            Result result = _cadastroService.CadastraUsuario(createDto);
+            return Ok();
+        }
+
+
+    }
+}
---
+++ FilmesAPI/UsuariosAPI/Services/CadastroService.cs
@@ -0,0 +1,14 @@
+﻿using MySqlX.XDevAPI.Common;
+using System;
+using UsuariosAPI.Data.Dtos;
+
+namespace UsuariosAPI.Services
+{
+    public class CadastroService
+    {
+        public Result CadastraUsuario(CreateUsuarioDto createDto)
+        {
+            
+        }
+    }
+}
