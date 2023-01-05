61e742f1b1622b0ceef279ae7bf400471f32fb1f
Luiz Fernando - Casa <36427462+LuizFernandoSousa@users.noreply.github.com>
2023-01-05 01:27:16 -0300

    Movie

---
+++ MovieAPI/MovieAPI/Controllers/MovieController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieAPI.Models;
+
+namespace MovieAPI.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+
+    public class MovieController : ControllerBase
+    {
+
+        private static List<Movie> movies = new List<Movie>();
+        private static int id = 0;
+
+        [HttpPost]
+        public IActionResult AddMovie([FromBody] Movie movie)
+        {
+            movie.Id = id++;
+            movies.Add(movie);
+            return CreatedAtAction(nameof(readMovieForID), new { id = movie.Id }, movie);
+            
+        }
+
+        [HttpGet]
+        public IEnumerable<Movie> readMovies([FromQuery] int skip = 0, int take = 20)
+        {
+            return movies.Skip(skip).Take(take);
+        }
+
+        [HttpGet("{id}")]   
+        public IActionResult readMovieForID(int id)
+        {
+            var movie =  movies.FirstOrDefault(movie => movie.Id == id);
+
+            if (movie == null)
+            {
+               return NotFound();
+            }
+            return Ok();
+
+        }
+
+
+    }
+}
---
+++ MovieAPI/MovieAPI/Models/Movie.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieAPI.Models;
+
+public class Movie
+{
+
+    public int Id { get; set; }
+
+
+    [Required(ErrorMessage ="The tittle is required!!")]
+    public string Tittle { get; set; }
+
+
+    [Required(ErrorMessage = " The Genre is required")]
+    [MaxLength(50,ErrorMessage = "The leagh of the genre cannot exceed 50 ")]
+    public string Genre  { get; set; }
+
+
+    [Required]
+    [Range(70,300, ErrorMessage = "The duration must between 70 and 300")]
+    public int Duration { get; set; }
+
+
+    [Required]
+    public string Director { get; set; }
+
+
+
+}
